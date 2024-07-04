160fc61d6b8119ed4afacfe02049939414ee1f0b
Ian Bennett <34078802+ian612@users.noreply.github.com>
2024-07-04 15:23:21 -0400

    Inital commit

---
+++ Tiled maps/House Exterior.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="House Exterior" tilewidth="16" tileheight="16" tilecount="450" columns="30">
+ <image source="Tileset images/GB Modular Houses.png" width="480" height="240"/>
+</tileset>
